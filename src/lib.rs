e40d3b3dbe46bb904b4f8ecc3dbe26983853055f
taaki <taaki.paul@gmail.com>
2023-03-26 11:17:55 -0400

    init

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+use std::fmt::{Display, Formatter, Result};
+
+// https://rsdlt.github.io/posts/rust-use-newtype-pattern-display-trait-array-generics/
+struct DispArray<T: Display, const N: usize>([T; N]);
+impl<T: Display, const N: usize> Display for DispArray<T, N> {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        let separator = ", ";
+        let mut string = String::from('[');
+        let last = self.0.last().unwrap();
+        for element in &self.0 {
+            string.push_str(&element.to_string());
+            if !std::ptr::eq(element, last) {
+                string.push_str(&separator);
+            }
+        }
+        string.push(']');
+        write!(f, "{string}")
+    }
+}
